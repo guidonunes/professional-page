18588d81b68743d601b6d621f6ccb97b5575053c
Guilherme Nunes <guilherme.augd@gmail.com>
2024-09-28 14:52:24 -0300

    update tailwind config and add styling to main section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main>
+    <main className='relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5'>
       <div>
         <h1>Hello Portfolio</h1>
       </div>
