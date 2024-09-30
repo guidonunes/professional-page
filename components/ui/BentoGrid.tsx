c3dfea5d4d5a195e587205c083b9f6048635f34c
Guilherme Nunes <guilherme.augd@gmail.com>
2024-09-30 16:22:34 -0300

    add list to my stack session

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -89,6 +89,19 @@ export const BentoGridItem = ({
         </div>
 
         {id === 2 && <GlobeDemo /> }
+
+        {id === 3 && (
+          <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+            <div className="flex flex-col gap-3 lg:gap-8">
+                {["Ruby on Rails", "React.js", "JavaScript"].map
+                ((item) => (
+                  <span>
+                    {item}
+                  </span>
+                ))}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
