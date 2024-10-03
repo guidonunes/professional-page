893f4639ef60092ad3d209efdf7a1c0f589474bd
Guilherme Nunes <guilherme.augd@gmail.com>
2024-10-03 18:32:04 -0300

    update navbar-styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,13 @@ import Grid from '../components/Grid';
 import FloatingNav from '../components/ui/FloatingNav';
 import { ImHome } from 'react-icons/im';
 import RecentProjects from '@/components/RecentProjects';
+import { navItems } from '@/data/index';
 
 export default function Home() {
   return (
     <main className='relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5'>
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[
-          {name: 'Home', link: '/', icon: <ImHome />},
-        ]}/>
+        <FloatingNav navItems={navItems}/>
         <Hero />
         <Grid />
         <RecentProjects />
